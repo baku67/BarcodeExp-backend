dd7a87ad43b286adb7032cd0405a6a9ef537d15b
baku67 <basile08@hotmail.fr>
2025-12-28 10:08:20 +0100

    Suppression de compte

--- Barcode backend/TODO.md
+++ Barcode backend/TODO.md
@@ -1,4 +1,22 @@
-Vue Register
+Fix Login/Register/deleteMe response et navigation (fonctionnent mais le front gère mal la réponse)
+
+
+Login/register: ajout titre page + centrer verticalement
+
+Backend: brut force et rate-limit surtout sur login/register
+tentatives de connexion + mail de confirmation (tant que pas validé ...)
+
+API Recette: OpenFoodFacts renvoie "Generic name" (dans plusieurs langues), liste des IA intéressantes: https://chatgpt.com/share/694fa79a-ad40-8005-8994-1980c8284a41
+
+Reorganiser File Structure Front
+
+"Nbr de prodsuits en DLC": count local ou BDD
+
+Loader du LoginForm
+Validation + grisage button LoginForm
+Validation + grisage button RegisterForm
+
+LoginForm+RegisterForm: Affichage Erreur UX
 
 Ajouter PGAdmin
 Ajouter UserFixture dans compose (admin admin admin)
--- Barcode backend/backend/src/Controller/MeController.php
+++ Barcode backend/backend/src/Controller/MeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MeController extends AbstractController
@@ -24,5 +26,22 @@ class MeController extends AbstractController
             'email' => $user->getUserIdentifier(), // ou getEmail() selon ton User
             'roles' => $user->getRoles(),
         ]);
+    }
+
+
+    #[Route('/me', name: 'me_delete', methods: ['DELETE'])]
+    public function deleteMe(EntityManagerInterface $em): Response
+    {
+        /** @var User|null $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            return new Response('', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $em->remove($user);
+        $em->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT); // ✅ 204 vide
     }
 }
